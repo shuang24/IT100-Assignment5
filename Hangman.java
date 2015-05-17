fd8d0950078123175c36825a812aebb2a5bbde8b
S <s>
2015-05-17 15:45:09 -0700

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -12,39 +12,39 @@ import acm.util.*;
 import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
-	
+
 	private String wordToGuess;
 	private String currentWord;
 	private HangmanCanvas canvas;
-	
+
 	public void init() {
 		canvas = new HangmanCanvas();
 		add(canvas);
 	}
 
-    public void run() {
+	public void run() {
 		println("Welcome to Hangman!");
-		
+
 		HangmanLexicon lexicon = new HangmanLexicon();
 		RandomGenerator rgen = RandomGenerator.getInstance();
-		
+
 		wordToGuess = lexicon.getWord(rgen.nextInt(lexicon.getWordCount()));
 		currentWord = dashifyWord();
 		boolean gameWon = false;
 		int numberOfGuessesLeft = 8;
 		canvas.reset();
-		
+
 		while (!gameWon && (numberOfGuessesLeft > 0)) {
 			canvas.displayWord(currentWord);
 		}
 	}
-    
-    private String dashifyWord() {
-    	String result = "";
-    	for (int i = 0; i < wordToGuess.length(); i++) {
-    		result += "-";
-    	}
-    	return result;
-    }
+
+	private String dashifyWord() {
+		String result = "";
+		for (int i = 0; i < wordToGuess.length(); i++) {
+			result += "-";
+		}
+		return result;
+	}
 
 }
